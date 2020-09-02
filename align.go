cd18458f130a0742126092703b5386c2d8f9680b
Ivan Egorov <vany.egorov@gmail.com>
2020-09-02 14:01:44 +0300

    Comment align enum.

--- align.go
+++ align.go
@@ -1,14 +1,17 @@
 package xstack
 
+// Align sets alignment of grid.
 type Align uint8
 
 const (
+	// AlignVertical is for vertical grid emission.
 	// For 6 inputs will produce 2x3 grid. e.g:
 	// | | |
 	// | | |
 	// | | |
 	AlignVertical Align = iota
 
+	// AlignHorizontal is for horizontal grid emission.
 	// For 6 inputs will produce 3x2 grid. e.g:
 	// | | | |
 	// | | | |
